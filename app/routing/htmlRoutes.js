2c4a7f0068cf950152122ea29ceda665564bf49d
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-02-09 13:02:08 +0800

    html routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,10 @@
 let path = require("path");
-module.exports = function(app) {};
+module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
