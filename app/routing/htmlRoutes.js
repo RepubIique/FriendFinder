7c3911da7fcf8f7c7b42647e9b40ef74168a1ff1
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-02-09 14:58:41 +0800

    html route fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,4 +7,8 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+
+  app.get("/assets/background.jpeg", function(req, res) {
+    res.sendFile(path.join(__dirname, "./../public/assets/background.jpeg"));
+  });
 };
