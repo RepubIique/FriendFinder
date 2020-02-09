61831f126c2d9c6f4984fabb47b08c82e9c50b28
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-02-09 15:50:18 +0800

    Calculation apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,24 +7,39 @@ module.exports = function(app) {
   fs.readFile(rawdata, (err, data) => {
     if (err) throw err;
     let friends = data.toString();
-    let friendsData = JSON.parse(friends);
+    let fData = JSON.parse(friends);
 
     app.get("/data/survey.json", function(req, res) {
       res.sendFile(path.join(__dirname, "/../data/survey.json"));
     });
 
     app.get("/api/friends", function(req, res) {
-      res.json(friendsData);
+      res.json(fData);
     });
-    // app.post("/api/friends", function(req, res) {
-    //   res.send(newUser);
-    // });
+    app.post("/api/friends", function(req, res) {
+      const { userscore } = req.body;
+      console.log("Req: ", userscore);
+      if (userscore) {
+        /* Do calculation once done send it back, replace userscore with the id  */
+        res.status(200).send(userscore);
+      } else {
+        res.status(500).send("Error");
+      }
+    });
 
     findFriend = user => {
-      for (let x = 0; x < friendsData.length; x++) {
-        for (let i = 0; i < friendsData[x].scores.length; i++) {}
+      for (let x = 0; x < fData.length; x++) {
+        for (let i = 0; i < fData[x].scores.length; i++) {
+          let totalArray = [];
+          let totalScore = 0;
+          if (fData[x].scores[i] <= user.scores[i]) {
+            totalScore += user.scores[i] - fData[i].scores[i];
+          } else {
+            totalScore += fData[x].scores[i] - user.scores[i];
+          }
+          totalArray.push(totalScore);
+        }
       }
     };
-    8;
   });
 };
